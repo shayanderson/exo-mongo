69fbc3fd660fdaac4076bdf5c71cc73c428d29f3
Shay Anderson <nexframe@yahoo.com>
2021-01-05 17:21:21 -0500

    mod docs

--- src/vendor/Exo2/Mongo/Store.php
+++ src/vendor/Exo2/Mongo/Store.php
@@ -3,7 +3,7 @@
  * Exo Mongo
  *
  * @package Exo
- * @copyright 2015-2020 Shay Anderson <https://www.shayanderson.com>
+ * @copyright 2015-2021 Shay Anderson <https://www.shayanderson.com>
  * @license MIT License <https://github.com/shayanderson/exo-mongo/blob/master/LICENSE>
  * @link <https://github.com/shayanderson/exo-mongo>
  */
